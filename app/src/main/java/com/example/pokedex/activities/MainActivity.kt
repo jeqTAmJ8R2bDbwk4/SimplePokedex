e32971255e3723d6504d1c275e42243c26181b37
lukas <felmyl98@zedat.fu-berlin.de>
2024-10-07 13:13:52 +0200

    Solve issue #1

--- app/src/main/java/com/example/pokedex/activities/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/activities/MainActivity.kt
@@ -2,29 +2,20 @@ package com.example.pokedex.activities
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.Insets
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.OneShotPreDrawListener
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
 import com.example.pokedex.databinding.ActivityMainNavigationBarBinding
-import com.example.pokedex.databinding.ActivityMainNavigationRailBinding
 import com.example.pokedex.utils.MainActivityInfo
 import com.example.pokedex.utils.isDarkMode
-import com.example.pokedex.utils.isLandscape
 import com.example.pokedex.viewmodels.MainActivityViewModel
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 import javax.inject.Inject
 
 
@@ -50,6 +41,14 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
         navController = navHostFragment.navController
         NavigationUI.setupWithNavController(binding.navigationView, navController)
+        binding.navigationView.setOnItemReselectedListener {
+            val startDestinationId = navController
+                .currentDestination
+                ?.parent
+                ?.startDestinationId
+                ?: return@setOnItemReselectedListener
+            navController.popBackStack(startDestinationId, inclusive = false)
+        }
 
         OneShotPreDrawListener.add(binding.navigationView) {
             mainActivityInfo.setBottomNavigationBarHeightPx(binding.navigationView.height)
